bf464d041c5f991bfdbdb43f58721b6bce132ace
Shi Yue <yueshi@cpe-76-175-118-100.socal.res.rr.com>
2017-09-12 04:03:20 -0700

    testMDP

--- SMC.py
+++ SMC.py
@@ -5,6 +5,11 @@ import math
 from abc import ABCMeta, abstractmethod
 from collections import defaultdict
 import random
+import csv
+import xlrd
+import re
+import copy
+from check import readFile
 class MDP:
 	def __init__(self,Q,alpha,beta,gamma,rho,m):
 		#self.Q=np.zeros((S,A))
@@ -40,6 +45,14 @@ class MDP:
 
 
 	def action_prob(self,s,a):
+		'''
+		Calculate the probability of taking action a in state s
+		Args:
+			s: state
+			a: action
+		Ouput:
+			Prob: probability of taking a in state s
+		'''
 		num=math.exp(self.beta*self.Q[s][a])
 		den=0.0
 		for i in range(len(self.Q[s])):
@@ -48,6 +61,13 @@ class MDP:
 		return prob
 
 	def selectAct(self,s):
+		'''
+		Select an action in state s
+		Args:
+			s: state
+		Output:
+		 	action
+		'''
 		prob_thresholds=[]
 
 		prob=0.0
@@ -65,7 +85,7 @@ class MDP:
 		a=self.selectAct(s)
 		s_next=self.transition(s,a)
 		r=self.reward_func(s,a,s_next)
-		delta= self.rho * r + self.gamma * np.max(self.Q[s])-self.Q[s][a]
+		delta= self.rho * r + self.gamma * np.max(self.Q[s_next])-self.Q[s][a]
 		self.Q[s][a]=self.m * Q[s][a]+ self.alpha * delta
 
 		#delta= r+self.gamma*np.max(self.Q[s])-self.Q[s][a]
@@ -80,6 +100,153 @@ gamma=0.3
 mdp=MDP(Q,alpha,beta,gamma)
 print mdp.selectAct(0)
 '''
+
+
+
+
+#Given history of state sequence S and action sequence A, calculate the transition probability using MLE
+#Here only binary S and A are considered.
+def calMLE(S,A):
+	tols0a0s0=0.0
+	tols0a0s1=0.0
+	tols0a1s0=0.0
+	tols0a1s1=0.0
+	tols1a0s0=0.0
+	tols1a0s1=0.0
+	tols1a1s0=0.0
+	tols1a1s1=0.0
+	total=len(S)
+	for i in range(len(S)-1):
+		if S[i]==0 and a[i]==0 and S[i+1]==0:
+			tols0a0s0 += 1
+		elif S[i]==0 and a[i]==0 and S[i+1]==1:
+			tols0a0s1 += 1
+		elif S[i]==0 and a[i]==1 and S[i+1]==0:
+			tols0a1s0 += 1
+		elif S[i]==0 and a[i]==1 and S[i+1]==1:
+			tols0a1s1 += 1
+		elif S[i]==1 and a[i]==0 and S[i+1]==0:
+			tols1a0s0 += 1
+		elif S[i]==1 and a[i]==0 and S[i+1]==1:
+			tols1a0s1 += 1
+		elif S[i]==1 and a[i]==1 and S[i+1]==0:
+			tols1a1s0 += 1
+		else:
+			tols1a1s1 += 1
+	probs0_s0a0=tols0a0s0/(tols0a0s0+tols0a0s1)
+	probs1_s0a0=tols0a0s1/(tols0a0s0+tols0a0s1)
+	probs0_s0a1=tols0a0s0/(tols0a1s0+tols0a1s1)
+	probs1_s0a1=tols0a0s1/(tols0a1s0+tols0a1s1)
+	probs0_s1a0=tols1a0s0/(tols1a0s0+tols1a0s1)
+	probs1_s1a0=tols1a0s1/(tols1a0s0+tols1a0s1)
+	probs0_s1a1=tols1a1s0/(tols1a1s0+tols1a1s1)
+	probs1_s1a1=tols1a1s1/(tols1a1s0+tols1a1s1)
+	Prob=np.arrary([probs0_s0a0,probs1_s0a0,probs0_s0a1,probs1_s0a1,probs0_s1a0,probs1_s1a0,probs0_s1a1,probs1_s1a1])
+	return Prob
+
+
+'''
+s=0, restricted env=0,s=1 restricted env=1
+a=0 , no drug, a=1 ,drug
+reward func: if s=0,a=0,Pr(r=1)=0.9
+if s=1,a=0,Pr(r=1)=0.7
+if s=0,a=1, Pr(r=1)=0.5
+if s=1,a=1,Pr(r=1)=0.1
+'''
+class testMDP(MDP):
+	def reward_func(self, s ,a ,s_next):
+		prob=random.random()
+		reward=0
+		if s==0 and a==0:
+			if prob<=0.9:
+				reward=1
+		elif s==1 and a==0:
+			if prob<=0.7:
+				reward=1
+		elif s==0 and a==1:
+			if prob<=0.5:
+				reward=1
+		else:
+			if prob<=0.1:
+				reward=1
+	def reward_dist(self, s ,a ,s_next,r):
+		prob=0.0
+		if s==0 and a==0:
+			if r==1:
+				prob=0.9
+			else:
+				prob=0.1
+		elif s==1 and a==0:
+			if r==1:
+				prob=0.7
+			else:
+				prob=0.3
+		elif s==0 and a==1:
+			if r==1:
+				prob=0.5
+			else:
+				prob=0.5
+		else:
+			if r==1:
+				prob=0.1
+			else:
+				prob=0.9
+		return prob
+
+	def transition_dist(self, s ,a ,s_next):
+		S,A=readFile("Exp_data_1.xlsx")
+		Prob=calMLE(S,A)
+		prob=0.0
+		if s==0 and a==0:
+			if s_next==0:
+				prob=Prob[0]
+			else:
+				prob=Prob[1]
+		elif s==0 and a==1:
+			if s_next==0:
+				prob=Prob[2]
+			else:
+				prob=Prob[3]
+		elif s==1 and a==1:
+			if s_next==0:
+				prob=Prob[4]
+			else:
+				prob=Prob[5]
+		elif s==1 and a==1:
+			if s_next==0:
+				prob=Prob[6]
+			else:
+				prob=Prob[7]
+		return prob						
+
+	def transition(self, s ,a):
+		S,A=readFile("Exp_data_1.xlsx")
+		Prob=calMLE(S,A)
+		prob=random.random()
+		if s==0 and a==0:
+			if prob<=Prob[0]:
+				s_next=0
+			else:
+				s_next=1
+		elif s==0 and a==1:
+			if prob<=Prob[2]:
+				s_next=0
+			else:
+				s_next=1
+		if s==1 and a==0:
+			if prob<=Prob[4]:
+				s_next=0
+			else:
+				s_next=1
+		if s==1 and a==1:
+			if prob<=Prob[6]:
+				s_next=0
+			else:
+				s_next=1
+		return s_next
+
+
+
 
 
 class SMC:
@@ -155,9 +322,19 @@ class SMC:
 				j += 1
 		new_samples=np.zeros(N)
 		for i  in range(N):
-			new_samples[i]=sample(indexes[i])
+			new_samples[i]=samples[indexes[i]]
 		return new_samples
 		#return indexes
+
+
+
+	def caleff(self,weights):
+		N=len(weights):
+		total=0.0
+		for weight in weights:
+			total += weight*weight
+		Neff=1.0/total
+		return Neff
 
 
 
--- check.py
+++ check.py
@@ -1,38 +1,87 @@
 import csv
 import xlrd
 import re
-p=re.compile('^tlfb_m\w\w_d\w\w_4$')
+import numpy as np
+# p=re.compile('^tlfb_m\w\w_d\w\w_4$')
+# q=re.compile('^tlfb_m\w\w_d\w\w_3$')
+
+# excelname1="Exp_data_1.xlsx"
+
+# workbook1 = xlrd.open_workbook(excelname1)
+
+# sheet1 = workbook1.sheet_by_index(0)
+
+# indexes=[]
+# cols0=sheet1.row_values(0)
+# for i in range(len(cols0)):
+# 	if p.match(cols0[i]):
+# 		indexes.append(i)
 
 
-excelname1="Exp_data_1.xlsx"
 
-workbook1 = xlrd.open_workbook(excelname1)
+# for row in range(1,sheet1.nrows):
+# 	val=0
+# 	tol=0
+# 	for col in range(sheet1.ncols):
 
-sheet1 = workbook1.sheet_by_index(0)
+# 		if col in indexes:
 
-indexes=[]
-cols0=sheet1.row_values(0)
-for i in range(len(cols0)):
-	if p.match(cols0[i]):
-		indexes.append(i)
-
-
-
-for row in range(1,sheet1.nrows):
-	val=0
-	tol=0
-	for col in range(sheet1.ncols):
-
-		if col in indexes:
-
-			tol += 1
-			if sheet1.cell(row,col).value:
+# 			tol += 1
+# 			if sheet1.cell(row,col).value:
 				
-				val += int(sheet1.cell(row,col).value)
-	print("row %d,  total number is: %d, non zero value is: %d." % (row,tol,val))
-	
+# 				val += int(sheet1.cell(row,col).value)
+# 	print("row %d,  total number is: %d, non zero value is: %d." % (row,tol,val))
 
 
+def readFile(filename):
+	p=re.compile('^tlfb_m\w\w_d\w\w_4$')
 
 
+	#xsexcelname1="Exp_data_1.xlsx"
+	excelname1=filename
 
+	workbook1 = xlrd.open_workbook(excelname1)
+
+	sheet1 = workbook1.sheet_by_index(0)
+
+	indexes=[]
+	cols0=sheet1.row_values(0)
+	for i in range(len(cols0)):
+		if p.match(cols0[i]):
+			indexes.append(i)
+
+
+	A=[]
+	for row in range(1,2):
+		for col in range(sheet1.ncols):
+			if col in indexes:
+				if sheet1.cell(row,col).value:	
+					A.append(int(sheet1.cell(row,col).value))
+				else:
+					A.append(0)
+	#print A
+	A=np.array(A)
+	q=re.compile('^tlfb_m\w\w_d\w\w_3$')
+	indexes=[]
+	cols0=sheet1.row_values(0)
+	for i in range(len(cols0)):
+		if q.match(cols0[i]):
+			indexes.append(i)
+
+
+	S=[]
+	for row in range(1,2):
+		for col in range(sheet1.ncols):
+			if col in indexes:
+				if sheet1.cell(row,col).value:	
+					S.append(int(sheet1.cell(row,col).value))
+				else:
+					S.append(0)
+	#print S
+
+	S=np.array(S)
+	return S,A
+
+# S,A=readFile("Exp_data_1.xlsx")
+
+# print S,A
